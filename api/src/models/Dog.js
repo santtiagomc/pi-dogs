002a812226f28be052c81d690d6b95b35efdb6a4
Santiago Moreno <santtiagomc@gmail.com>
2022-08-16 10:19:25 -0500

    primeros pasos

---
+++ api/src/models/Dog.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('dogs', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primarykey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    life_span: {
+      type: DataTypes.STRING
+    },
+    image: {
+      type: DataTypes.TEXT,
+    }
+  }, {
+    timestamps: false
+  });
+};
